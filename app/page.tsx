51bc109fa9c497e1aef114e16528aa24438aac4e
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-04-04 23:37:46 +0530

    feat: add Testimonials component and TestimonialsColumns for user feedback section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import { GalleryShowcaseSection } from "@/components/GalleryShowCaseSection";
 import {HomeHeroSection} from "@/components/HomeHeroSection";
 import { HowItWorksSection } from "@/components/HowItWorksSection";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background p-3 sm:p-4 lg:p-5">
 
-    <HomeHeroSection />
-    <GalleryShowcaseSection />
-    <HowItWorksSection />
+      <HomeHeroSection />
+      <GalleryShowcaseSection />
+      <HowItWorksSection />
+      <Testimonials />
+
     </main>
   );
 } 
